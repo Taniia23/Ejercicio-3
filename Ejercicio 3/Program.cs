e795c7fe10decef7be24afec5e48bb213a8bfa6b
Taniia23 <164653734+Taniia23@users.noreply.github.com>
2025-02-14 23:33:05 -0600

    Agregar archivos de proyecto.

---
+++ Ejercicio 3/Program.cs
@@ -0,0 +1,26 @@
+﻿/* Tania Garcia*/
+
+using System;
+
+class Ejercicio3
+{
+    public static void Main(string[] args)
+    {
+        double n = 9.75;
+        int n1 = 7;
+
+        //CONVERSION DE DOUBLE A INT
+        int doubleint = Convert.ToInt32(n);
+
+        //CONVERSION DE INT A DOUBLE
+        double con = Convert.ToDouble(n1);
+
+        Console.WriteLine("Valor original double: " + n);
+        Console.WriteLine("Double convertido a int :" + doubleint);
+        Console.WriteLine("Valor original int: " + n1);
+        Console.WriteLine("Int convertido a double: " + con);
+
+    }
+
+
+}
